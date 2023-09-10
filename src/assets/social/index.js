31260f6580199723c1bcb16b1b38b3fd16766ba2
albertchitta <albert.w.chittaphong@gmail.com>
2023-09-10 01:40:50 -0500

    update footer with social and resume

---
+++ src/assets/social/index.js
@@ -0,0 +1,13 @@
+import facebook from "./facebook.png";
+import twitter from "./twitter.png";
+import instagram from "./instagram.png";
+import github from "./github.png";
+import linkedin from "./linkedin.png";
+
+export {
+  facebook,
+  twitter,
+  instagram,
+  github,
+  linkedin,
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,40 @@
+import { socialLinks } from "../constants";
+
 const Footer = () => {
   return (
     <footer class="bg-black-100 rounded-lg shadow dark:bg-gray-900">
       <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-          <div class="sm:flex sm:items-center sm:justify-between">
-              <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
-                  <img src="/favicon.ico" class="h-8 mr-3" alt="Albert Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Albert</span>
-              </a>
-              <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                  <li>
-                      <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-                  </li>
-                  <li>
-                      <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                  </li>
-                  <li>
-                      <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
-                  </li>
-                  <li>
-                      <a href="#" class="hover:underline">Contact</a>
-                  </li>
-              </ul>
-          </div>
-          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">Copyright © 2023 <a href="https://flowbite.com/" class="hover:underline">Albert Chittaphong</a>. All Rights Reserved.</span>
+        <div class="flex items-center flex-col">
+          <a
+            href="Albert_Chittaphong_Resume.pdf"
+            download
+            className="bg-tertiary border-2 rounded-full py-3 px-6 hover:bg-white hover:text-black transition ease-in-out duration-300"
+          >
+            Download Resume
+          </a>
+
+          <ul class="mt-6 flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            {socialLinks.map((social) => (
+              <li key={social.name}>
+                <a
+                  aria-label={social.name}
+                  href={social.link}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={social.icon}
+                    alt={social.name}
+                    className="w-1/2 h-1/2 object contain transform hover:scale-125 transition ease-in-out duration-300"
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* TODO: Add URL here */}
+        <span class="flex justify-center mt-6 block text-sm text-gray-500 sm:text-center dark:text-gray-400">Copyright © 2023&nbsp;<a href="#/" class="hover:underline">Albert Chittaphong</a>. All Rights Reserved.</span>
       </div>
     </footer>
   )
